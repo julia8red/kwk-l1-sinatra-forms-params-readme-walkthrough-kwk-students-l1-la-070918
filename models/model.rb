ff34a1710ae03c75054ef7116645afc7546f1496
Julia Cuervo <juliahcuervo@gmail.com>
2018-07-18 23:28:50 +0000

    automatically backed up by learn

--- models/model.rb
+++ models/model.rb
@@ -1,23 +1,40 @@
-#this is were our ruby goes
-def get_animal(animal,hat)
-  if animal == "dog"
-    if hat == "party-hat"
-      animal_img = "https://www.warrenphotographic.co.uk/photography/bigs/30873-Shar-pei-pup-wearing-a-birthday-party-hat-white-background.jpg"
-    elsif hat == "top-hat"
-      animal_img = "https://s-media-cache-ak0.pinimg.com/originals/f1/b0/5c/f1b05c39afe6f55975215f93b1272aef.jpg"
-    elsif hat == "bucket-hat"
-      animal_img = "https://i.pinimg.com/236x/9b/c1/bc/9bc1bc873f474f27f22942ceec9ea1be--cute-hats-silly-hats.jpg"
-    end
-  elsif animal == "cat"
-    if hat == "party-hat"
-      animal_img = "http://cutecatsinhats.com/wp-content/uploads/2016/01/first-birthday-kitten.jpg"
-    elsif hat == "top-hat"
-      animal_img = "https://i.imgur.com/LuR9u1A.jpg"
-    elsif hat == "bucket-hat"
-      animal_img = "https://78.media.tumblr.com/tumblr_lotd24oxjA1qdvbl3o1_500.jpg"
-    end
-  end
-  return animal_img
-end
 
-puts get_animal("dog","party-hat")
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Quiz</title>
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+  </head>
+  <body background="http://s7.favim.com/orig/150107/background-emoji-wallpaper-emoji-wallpaper-Favim.com-2366197.jpg">
+    <h1> What content do you want to upload?</h1>
+
+  <form method="post" action="/end_page">
+    <input type="radio" name="content" value="image" checked> Image <br>
+    <input type="radio" name="content" value="text" checked> Text <br>
+    <input type="radio" name="content" value="video" checked> Video <br>
+    <h3> Who do you want to see this? </h3>
+
+    <input type="radio" name="audience" value="family_and_friends" checked> Family and Friends <br>
+    <input type="radio" name="audience" value="everyone" checked> Everyone <br>
+    <input type="radio" name="audience" value="business" checked> Business <br>
+      
+    <h3> What is the level of importance? </h3>
+
+    <input type="radio" name="importance" value="1" checked> 1 <br>
+    <input type="radio" name="importance" value="5" checked> 5 <br>
+    <input type="radio" name="importance" value="10" checked> 10 <br>
+
+    <h3> What is the purpose of the post? </h3>
+    <input type="radio" name="purpose" value="funny" checked> Funny <br>
+    <input type="radio" name="purpose" value="serious" checked> Serious <br>
+    <input type="radio" name="purpose" value="for_fun" checked> For Fun <br>
+    <input type="radio" name="purpose" value="event" checked> Event <br>
+    <input type="radio" name="purpose" value="artsy" checked> Artsy <br>
+
+    <h3>When you're done Submit it</h3>
+
+    <a href="end_page"><button type="submit">Submit</button></a>
+  </form>
+</body>
+
+</html>
