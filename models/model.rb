1a69325dce1bc420fbfedce7c060f9a690e7786c
Julia Cuervo <juliahcuervo@gmail.com>
2018-07-18 23:33:36 +0000

    automatically backed up by learn

--- models/model.rb
+++ models/model.rb
@@ -1,40 +1,65 @@
-<!doctype html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Quiz</title>
-    <link rel="stylesheet" type="text/css" href="../css/styles.css">
-  </head>
-  <body background="http://s7.favim.com/orig/150107/background-emoji-wallpaper-emoji-wallpaper-Favim.com-2366197.jpg">
-    <h1> What content do you want to upload?</h1>
 
-  <form method="post" action="/end_page">
-    <input type="radio" name="content" value="image" checked> Image <br>
-    <input type="radio" name="content" value="text" checked> Text <br>
-    <input type="radio" name="content" value="video" checked> Video <br>
-    <h3> Who do you want to see this? </h3>
+h5 {
+  text-align: center;
+  font-size: 48pt;
+  margin-bottom: 5%;
+  /*margin: 0 auto;*/
+  font-family: 'Rock Salt', cursive;
+  /*color: #660066;*/
+  background-color: #ffffff;
+  /*width: 28.5%;*/
+}
 
-    <input type="radio" name="audience" value="family_and_friends" checked> Family and Friends <br>
-    <input type="radio" name="audience" value="everyone" checked> Everyone <br>
-    <input type="radio" name="audience" value="business" checked> Business <br>
-      
-    <h3> What is the level of importance? </h3>
+h1 {
+  text-align: center;
+  height: 20%;
+  font-size: 50px;
+  margin: 0 auto;
+  font-family: 'Rancho', cursive;
+  /*color: #660066;*/
+  background-color: #ffffff;
+  width: 32%
+}
+a {
+  display: block;
+  width: 100%;
+  text-align: center;
+}
+button {
+  margin: 0 auto;
+  background-color: #9933ff;
+   border: 1px solid;
+   font-size: 24px;
+   color: white;
+   padding: 15px 32px;
+   text-align: center;
+   text-decoration: none;
+   /*display: inline-block;*/
+}
 
-    <input type="radio" name="importance" value="1" checked> 1 <br>
-    <input type="radio" name="importance" value="5" checked> 5 <br>
-    <input type="radio" name="importance" value="10" checked> 10 <br>
+form {
+  width: 32%;
+  background-color: #ffffff;
+  margin: 0 auto;
+  font-size: 40px;
+  font-family: 'Rancho', cursive;
+}
+h2{
+  text-align: center;
+  height: 20%;
+  font-size: 50px;
+  margin: 0 auto;
+  font-family: 'Rancho', cursive;
+  background-color: #ffffff;
+  width: 32%
+}
 
-    <h3> What is the purpose of the post? </h3>
-    <input type="radio" name="purpose" value="funny" checked> Funny <br>
-    <input type="radio" name="purpose" value="serious" checked> Serious <br>
-    <input type="radio" name="purpose" value="for_fun" checked> For Fun <br>
-    <input type="radio" name="purpose" value="event" checked> Event <br>
-    <input type="radio" name="purpose" value="artsy" checked> Artsy <br>
+h3 {
+  text-align: center;
+  height: 20%;
+  font-size: 50px;
+  font-family: 'Rancho', cursive;
+  background-color: #ffffff;
+  /*width: 32%*/
+}
 
-    <h3>When you're done Submit it</h3>
-
-    <a href="end_page"><button type="submit">Submit</button></a>
-  </form>
-</body>
-
-</html>
